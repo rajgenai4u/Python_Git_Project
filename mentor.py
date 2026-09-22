1829645c7417b4b06579f2663958c8efdf2db502
Karre  Rajesh <rajsai4us@gmail.com>
2026-09-22 13:51:16 +0800

    Updated Mentor file

--- mentor.py
+++ mentor.py
@@ -3,6 +3,4 @@ from user import User
 class Mentor(User):
     def display_details(self):
         super().display_details()
-        print("Role: Mentor")
-    
-    
+        print("Role : Mentor")
